fd0da33335d2627bf6bf1522ee8e38aa413d6dc7
Bhanu <kalavakolanulakshmi2004@gmil.com>
2025-05-04 10:00:00 +0530

    add day 3 files

---
+++ Day3/greetingapp/src/main/java/com/example/greetingapp/model/Greeting.java
@@ -0,0 +1,37 @@
+// Greeting.java
+package com.example.greetingapp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Greeting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    public Greeting() {}
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public void setMessage(String newMsg) {
+        this.message = newMsg;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    // Getters and Setters
+}
