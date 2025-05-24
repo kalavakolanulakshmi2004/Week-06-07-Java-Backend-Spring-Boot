4079928b4b5fc7bf888e707996d61935de39f7b4
Bhanu <kalavakolanulakshmi2004@gmil.com>
2025-05-24 11:57:07 +0530

    delete

--- Day5/BankingApplication1/src/main/java/com/example/BankingApplication1/AccountOperations.java
+++
@@ -1,6 +0,0 @@
-package com.example.BankingApplication1;
-public interface AccountOperations {
-    void deposit(double amount);
-    void withdraw(double amount);
-    double getBalance();
-}
--- Day5/BankingApplication1/src/main/java/com/example/BankingApplication1/BankRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.BankingApplication1;
-import com.example.BankingApplication1.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankRepository extends JpaRepository<Bank, Long> {
-}
